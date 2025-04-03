c7c157adfe10fda1178c13b8ad07f8164fd39cac
iknowmagic <ilan@iknowmagic.net>
2025-04-03 13:25:23 -0700

    feat: add React Router for navigation and update ESLint configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from './components/Home'
+// import About from './components/About'
 
 function App() {
-  return <Home />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
